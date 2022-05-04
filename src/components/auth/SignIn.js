5a92eb2f80b90cd3cc423827cb2eb6cf0718c212
yazeedhani <haniyazeed9@gmail.com>
2022-05-03 18:51:29 -0600

    Styled sign-in view.

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,11 +1,23 @@
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 
 import { signIn } from '../../api/auth'
 import messages from '../shared/AutoDismissAlert/messages'
 
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
+import { Form, Button } from 'react-bootstrap'
+
+const inputStyle = {
+    width: 400,
+}
+
+const buttonStyle = {
+    width: 400,
+}
+
+const formStyle = {
+    position: 'absolute', left: '50%', top: '30%',
+    transform: 'translate(-50%, -50%)'
+}
 
 const SignIn = (props) => {
     const [username, setUsername] = useState('')
@@ -42,7 +54,7 @@ const SignIn = (props) => {
 	}
 
     return (
-        <div className='row'>
+        <div className='row'style={formStyle}>
             <div className='col-sm-10 col-md-8 mx-auto mt-5'>
                 <h3>Sign In</h3>
                 <Form onSubmit={onSignIn}>
@@ -50,28 +62,36 @@ const SignIn = (props) => {
                         <Form.Label>Username</Form.Label>
                         <Form.Control
                             required
+                            style={inputStyle}
                             type='text'
                             name='username'
                             value={username}
                             placeholder='Enter username'
+                            className='sign-in-form-input'
                             onChange={e => setUsername(e.target.value)}
                         />
                     </Form.Group>
+                    <br/>
                     <Form.Group controlId='password'>
                         <Form.Label>Password</Form.Label>
                         <Form.Control
                             required
+                            style={inputStyle}
                             name='password'
                             value={password}
                             type='password'
-                            placeholder='Password'
+                            placeholder='Enter password'
+                            className='sign-in-form-input'
                             onChange={e => setPassword(e.target.value)}
                         />
                     </Form.Group>
-                    <Button variant="outline-success" type='submit'>
+                    <br/>
+                    <Button style={buttonStyle} variant="outline-success" type='submit'>
                         Submit
                     </Button>
                 </Form>
+                <br/>
+                <span>Don't have an account yet? <Link to='/sign-up' style={{ textDecoration: 'none' }}>Sign up</Link> </span>
             </div>
         </div>
     )
